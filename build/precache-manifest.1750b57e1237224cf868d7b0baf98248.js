4824ce67da430a6719f93dd0b8acd92134fafc18
main.email.seryoga <main.email.seryoga@gmail.com>
2020-03-30 21:20:20 +0300

    build

---
+++ build/precache-manifest.1750b57e1237224cf868d7b0baf98248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77d4469b0385dff88058368682250187",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93b4f5ec44b066e550ec",
+    "url": "/static/css/main.421610ad.chunk.css"
+  },
+  {
+    "revision": "08eba7a8334325e3e73a",
+    "url": "/static/js/2.277feef6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.277feef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93b4f5ec44b066e550ec",
+    "url": "/static/js/main.4ed9966b.chunk.js"
+  },
+  {
+    "revision": "adcc072313e2edd11c2c",
+    "url": "/static/js/runtime-main.665367fb.js"
+  }
+]);
