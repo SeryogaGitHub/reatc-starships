2af006f9e405b90150469004960806ea50c378bf
main.email.seryoga <main.email.seryoga@gmail.com>
2020-04-04 17:07:36 +0300

    вав

---
+++ precache-manifest.7cd015fe0ef16b471e08fe89fb8f63ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a53874be748f76e0e3efea93e3f4cdc",
+    "url": "/starships/index.html"
+  },
+  {
+    "revision": "ca61bb64b87207bb3e5c",
+    "url": "/starships/static/css/main.cf5d0ebf.chunk.css"
+  },
+  {
+    "revision": "049eb0e97be1119f4fce",
+    "url": "/starships/static/js/2.118c8700.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/starships/static/js/2.118c8700.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca61bb64b87207bb3e5c",
+    "url": "/starships/static/js/main.6220c5c4.chunk.js"
+  },
+  {
+    "revision": "a197b553894a2ff425d5",
+    "url": "/starships/static/js/runtime-main.d887b3b5.js"
+  }
+]);
